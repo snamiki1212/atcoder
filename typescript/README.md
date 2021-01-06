80115bdeecc298caf19f85d6231540f7c085bf2d
snamiki1212 <snamiki1212@gmail.com>
2021-01-05 20:52:29 -0800

    Update ts env

---
+++ typescript/README.md
@@ -0,0 +1,7 @@
+## TypeScript
+
+```zsh
+$ yarn dev:input
+```
+
+- `./input.txt`の値を`./src/index.ts`に標準入力として受け取って処理
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import * as fs from "fs";
+// parser for input
+const input = fs.readFileSync("/dev/stdin", "utf8").split("\n");
+const [_N, ...rest] = input;
+const N = parseInt(_N);
+const lines = rest.map((_line) => {
+  return _line.split(" ").map((_) => parseInt(_));
+});
+
+// Start logic....
+console.log("hello", N, lines);
