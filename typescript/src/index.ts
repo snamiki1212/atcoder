092893f1103d4ccfae7f30cd0d10d1857b7e5218
snamiki1212 <snamiki1212@gmail.com>
2021-04-23 20:55:08 -0700

    Update

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,11 +1,16 @@
 import * as fs from "fs";
-// parser for input
 const input = fs.readFileSync("/dev/stdin", "utf8").split("\n");
+
+// --------------------------------
+// Parse
+// --------------------------------
 const [_N, ...rest] = input;
 const N = parseInt(_N);
 const lines = rest.map((_line) => {
   return _line.split(" ").map((_) => parseInt(_));
 });
 
+// --------------------------------
 // Start logic....
+// --------------------------------
 console.log("hello", N, lines);
